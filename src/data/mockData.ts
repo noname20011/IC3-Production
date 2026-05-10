5f0ab8bc059a92d7bda7e18a6a7777fff4f797aa
no_name <hieuddeu@gmail.com>
2026-05-10 23:26:25 +0700

    update add question

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -3,15 +3,54 @@ import { QuizLevel, School } from "../../types";
 
 export const MOCK_LEVELS: QuizLevel[] = [
   {
-    id: "a263a204-149d-42a0-a7fa-75646e7e3516",
+    id: "1a3890c5-96dd-4c81-a337-7dc38e82558a",
     name: "Level 1",
     description: "Foundational concepts and basic knowledge.",
     parts: [
-      { id: "1d21942d-99d5-47cb-befe-8e784741956b", name: "Bổ Sung 1", levelId: "1a3890c5-96dd-4c81-a337-7dc38e82558a", description: "Phần ôn tập sổ sung số 1", duration: 2000, questionCount: 12 },
-      { id: "1d21942d-99d5-47cb-befe-8e784741956c", name: "Bổ Sung 2", levelId: "a263a204-149d-42a0-a7fa-75646e7e3516", description: "Phần ôn tập sổ sung số 2", duration: 2000, questionCount: 22 },
-      { id: "1d21942d-99d5-47cb-befe-8e784741956d", name: "OT1", levelId: "a263a204-149d-42a0-a7fa-75646e7e3516", description: "Ôn tập tổng hợp số 1", duration: 2700, questionCount: 45 },
-      { id: "1d21942d-99d5-47cb-befe-8e784741956e", name: "OT2", levelId: "a263a204-149d-42a0-a7fa-75646e7e3516", description: "Ôn tập tổng hợp số 2", duration: 2700, questionCount: 45 },
-      { id: "1d21942d-99d5-47cb-befe-8e784741956f", name: "OT3", levelId: "a263a204-149d-42a0-a7fa-75646e7e3516", description: "Ôn tập tổng hợp số 3", duration: 2700, questionCount: 58 },
+      {
+        id: "61fa749d-4122-4504-b5e7-c4bdd65984a7",
+        name: "Bổ Sung 1",
+        levelId: "1a3890c5-96dd-4c81-a337-7dc38e82558a",
+        description: "Phần ôn tập sổ sung số 1",
+        duration: 2000,
+        questionCount: 12,
+      },
+
+      {
+        id: "eaad19b7-3965-44c3-a1fd-cbb0d3e3ce72",
+        name: "Bổ Sung 2",
+        levelId: "1a3890c5-96dd-4c81-a337-7dc38e82558a",
+        description: "Phần ôn tập sổ sung số 2",
+        duration: 2000,
+        questionCount: 22,
+      },
+
+      {
+        id: "d051e1f8-2410-4c53-8650-438afbcdd894",
+        name: "OT1",
+        levelId: "1a3890c5-96dd-4c81-a337-7dc38e82558a",
+        description: "Ôn tập tổng hợp số 1",
+        duration: 2700,
+        questionCount: 45,
+      },
+
+      {
+        id: "005fc9a6-fb8d-46c0-9f45-a36cf54be110",
+        name: "OT2",
+        levelId: "1a3890c5-96dd-4c81-a337-7dc38e82558a",
+        description: "Ôn tập tổng hợp số 2",
+        duration: 2700,
+        questionCount: 45,
+      },
+
+      {
+        id: "94160047-c31a-4388-967b-215b8378fa2a",
+        name: "OT3",
+        levelId: "1a3890c5-96dd-4c81-a337-7dc38e82558a",
+        description: "Ôn tập tổng hợp số 3",
+        duration: 2700,
+        questionCount: 58,
+      },
     ],
   },
   {
@@ -38,7 +77,14 @@ export const MOCK_LEVELS: QuizLevel[] = [
     name: "Practice Test",
     description: "Full-length simulation of the final assessment.",
     parts: [
-      { id: "1d21942d-99d5-47cb-befe-8e784741956a", name: "Random Test Level 1", levelId: "practice", description: "45 câu ngẫu nhiên của Level 1", duration: 2700, questionCount: 45  },
+      {
+        id: "1d21942d-99d5-47cb-befe-8e784741956a",
+        name: "Random Test Level 1",
+        levelId: "practice",
+        description: "45 câu ngẫu nhiên của Level 1",
+        duration: 2700,
+        questionCount: 45,
+      },
       // { id: 10, name: "Mock B", levelId: "practice" },
     ],
   },
@@ -51,8 +97,32 @@ export const MOCK_SCHOOLS: School[] = [
   { id: "s4", name: "THCS Trần Đại Nghĩa", location: "Boston" },
 ];
 
-export const MOCK_LEADERBOARD: LeaderBoard[]  = [
-  { id: "u1", studentName: "Tôn Nữ Tú Anh", score: 450, time_spent: 80, school_name: "THCS Huỳnh Văn Nghệ", class_name: "6A4", rank: 1 },
-  { id: "u2", studentName: "Nguyễn Linh Anh", score: 450, time_spent: 82, school_name: "THCS Yên Thế", class_name: "7A1", rank: 2 },
-  { id: "u3", studentName: "Hồ Quang Tùng", score: 450, time_spent: 82, school_name: "THCS Huỳnh Văn Nghệ", class_name: "8A2", rank: 3 },
+export const MOCK_LEADERBOARD: LeaderBoard[] = [
+  {
+    id: "u1",
+    studentName: "Tôn Nữ Tú Anh",
+    score: 450,
+    time_spent: 80,
+    school_name: "THCS Huỳnh Văn Nghệ",
+    class_name: "6A4",
+    rank: 1,
+  },
+  {
+    id: "u2",
+    studentName: "Nguyễn Linh Anh",
+    score: 450,
+    time_spent: 82,
+    school_name: "THCS Yên Thế",
+    class_name: "7A1",
+    rank: 2,
+  },
+  {
+    id: "u3",
+    studentName: "Hồ Quang Tùng",
+    score: 450,
+    time_spent: 82,
+    school_name: "THCS Huỳnh Văn Nghệ",
+    class_name: "8A2",
+    rank: 3,
+  },
 ];
